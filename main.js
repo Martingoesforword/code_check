77ede47b08b7b9fef98d3ab7d1e87e07a752382a
zenzen <song8713@qq.com>
2022-04-07 18:49:27 +0800

    基本完成获取git提交文件列表，退出功能等

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 //从githook获取提交记录
 
-
+var process = require('process');
+var cmd=require('node-cmd');
+var ret = cmd.runSync('git diff --cached --name-only');
+var changeFiles = ret.data.split("\n");
 // 从提交记录获取变动文件列表
+var errors = [];
+var warrings = [];
 
+// 可配置过滤器组合：向过滤器传入参数
 
 // 对某种文件名，文件大小等信息实现自定义劫持操作，或者通用劫持操作
 // fileName.match() 和 customMatch(fileInfo)
@@ -13,3 +19,11 @@
 
 
 // 输出信息列表
+
+
+if(errors.length || warrings.length) {
+    process.exit(-1);
+}
+else {
+    process.exit(0);
+}
